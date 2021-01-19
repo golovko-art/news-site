5772b5e3c8a40a69b18ceae5777d6ba552f72f68
Artem Golovko <golovko.up@gmail.com>
2021-01-19 23:21:22 +0300

    Starting develop branch

--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
+
 
 def index(request):
-    return HttpResponse('Hello world')
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
