bc858ec81dc90d4b91430821080a392c5a4590a1
Artem Golovko <golovko.up@gmail.com>
2021-01-22 13:42:05 +0300

    Add category

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from news.views import *
+from news import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
+    path('category/<int:category_id>/', views.get_category, name='category'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
+    categories = Category.objects.all()
+    return render(request, 'news/index.html', {'news': news,
+                                               'title': 'Список новостей',
+                                               'categories': categories})
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news,
+                                                  'categories': categories,
+                                                  'category': category})
