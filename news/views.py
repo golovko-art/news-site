0bba39972d4b3288ded2448c6e84b7eb1cc1545b
Artem Golovko <golovko.up@gmail.com>
2021-01-23 22:01:00 +0300

    Add custom template tags

--- news/views.py
+++ news/views.py
@@ -5,16 +5,12 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     return render(request, 'news/index.html', {'news': news,
-                                               'title': 'Список новостей',
-                                               'categories': categories})
+                                               'title': 'Список новостей'})
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news,
-                                                  'categories': categories,
                                                   'category': category})
