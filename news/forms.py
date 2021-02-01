44d522383dab32e7f5c68822de8fca46d28bc2ff
Artem Golovko <golovko.up@gmail.com>
2021-02-01 13:17:48 +0300

    Start working with forms

---
+++ news/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import News
+
+""" Форма не связаная с моделью ('пример' отправка пимьма на емейл) """
+
+
+# class NewsForm(forms.Form):
+#     title = forms.CharField(label='Название', max_length=200, widget=forms.TextInput(attrs={"class": "form-control"}))
+#     content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={
+#         "class": "form-control",
+#         "rows": 5
+#     }))
+#     is_published = forms.BooleanField(label='Опубликовано?', initial=True)
+#     category = forms.ModelChoiceField(empty_label='Выберете категорию', label='Категория',
+#                                       queryset=Category.objects.all(),
+#                                       widget=forms.Select(attrs={"class": "form-control"}))
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = [
+            'title',
+            'content',
+            'is_published',
+            'category'
+        ]
+        widgets = {
+            'title': forms.TextInput(attrs={"class": "form-control"}),
+            'content': forms.Textarea(attrs={"class": "form-control", "rows": 5}),
+            'category': forms.Select(attrs={"class": "form-control"}),
+        }
--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('category/<int:category_id>/', views.get_category, name='category'),
     path('news/<int:news_id>/', views.view_news, name='view_news'),
+    path('news/add_news/', views.add_news, name='add_news'),
 ]
